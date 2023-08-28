c936b31d7b47dd1659066567d2c171df2b11f83f
=Faizan Ahmad <=faizanhafiz928@gmail.com>
2023-08-28 15:04:48 +0530

    added application.yml

--- src/main/java/com/lazeez/Main.java
+++ src/main/java/com/lazeez/Main.java
@@ -1,7 +1,30 @@
 package com.lazeez;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableWebMvc
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+
     }
-}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Allow any origin
+                        .allowedMethods("*") // Allow any HTTP method (GET, POST, etc.)
+                        .allowedHeaders("Content-Type", "Authorization"); // Allow specific headers
+            }
+        };
+}}
